83e123580865b7ae03363d01edfdd931825b8496
AAKhan4 <a.a.khan8@lancaster.ac.uk>
2024-06-26 19:51:36 +0200

    Create Info.js

---
+++ portfolio/src/data/Info.js
@@ -0,0 +1,14 @@
+export const Bio = {
+  name: "Aditya Khan",
+  roles: [
+    "Software Engineering Student",
+    "Full Stack Developer",
+    "Programmer",
+  ],
+  description:
+    "Final year Software Engineering student at Lancaster University. Highly motivated self-sufficient learner, dedicated to expanding my skill set through independent research & online courses both within & beyond the scope of my university curriculum. Seeking internship opportunities to apply current knowledge, learn, contribute & gain real life experience working in a professional environment.",
+  github: "https://github.com/AAKhan4",
+  linkedin: "https://www.linkedin.com/in/aditya-a-khan/",
+  email: "aditya041004@gmail.com",
+  resume: "PLACEHOLDER",
+};
