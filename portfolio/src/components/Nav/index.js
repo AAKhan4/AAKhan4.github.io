2ab459871176456fe3e20193fdca1728d2e2a26f
AAKhan4 <a.a.khan8@lancaster.ac.uk>
2024-06-26 13:29:03 +0200

    Update index.js

--- portfolio/src/components/Nav/index.js
+++ portfolio/src/components/Nav/index.js
@@ -77,6 +77,37 @@ const NavLink = styled.a`
   }
 `;
 
+const ButtonContainer = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: end;
+    width: 80%;
+    height: 100%;
+    padding: 0 6px;
+    @media screen and (max-width: 640px) {
+        display: none;
+    }
+`;
+
+const GithubButton = styled.button`
+    border: 1.8px solid ${({ theme }) => theme.primary};
+    border-radius: 20px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 0px 20px;
+    font-size: 1rem;
+    font-weight: 500;
+    cursor: pointer;
+    height: 70%;
+    :hover {
+        background-color: ${({ theme }) => theme.primary}};
+    }
+    @media screen and (max-width: 640px) {
+        font-size: 0.8rem;
+    }
+`;
+
 export default function Navbar() {
   return (
     <Nav>
