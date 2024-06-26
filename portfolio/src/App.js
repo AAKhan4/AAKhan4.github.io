78b2ba23d284ae48007ffc021be7cf6329632ea7
AAKhan4 <a.a.khan8@lancaster.ac.uk>
2024-06-26 12:01:21 +0200

    Update App.js

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,7 +1,20 @@
 import "./App.css";
+import styled, { ThemeProvider } from "styled-components";
+import { darkTheme } from "./utils/Themes";
+
+const Body = styled.div`
+  background-color: ${({ theme }) => theme.bg};
+  height:100%;
+  width: 100%;
+  overflow-x: hidden;
+`;
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <Body></Body>
+    </ThemeProvider>
+  );
 }
 
 export default App;
