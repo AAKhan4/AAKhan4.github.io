b7e5c9b6a4c9c0aa9a2860f7415a761bfc50a92d
AAKhan4 <a.a.khan8@lancaster.ac.uk>
2024-06-26 13:18:05 +0200

    Update index.js

--- portfolio/src/components/Nav/index.js
+++ portfolio/src/components/Nav/index.js
@@ -1,5 +1,73 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+import styled from "styled-components";
 
-export default function Nav() {
-    return <div>Navbar</div>
+const Nav = styled.div`
+  background-color: ${({ theme }) => theme.card_Light};
+  height: 80px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 1rem;
+  position: sticky;
+  top: 0;
+  z-index: 10;
+  @media screen and (max-width: 960px) {
+    transition: 0.8s all ease;
+  }
+`;
+
+const NavContainer = styled.div`
+  display: flex;
+  justified-content: space-between;
+  height: 60px;
+  z-index: 1;
+  width: 100%;
+  padding: 0 24px;
+  max-width: 1200px;
+`;
+
+const NavLogo = styled(Link)`
+  width: 80%;
+  padding: 0 6px;
+  display: flex;
+  justify-self: flex-start;
+  cursor: pointer;
+  text-decoration: none;
+  align-items: center;
+  @media screen and (max-width: 640px) {
+    padding: 0 0px;
+  }
+`;
+
+const MobileIcon = styled.div`
+  display: none;
+  @media screen and (max-width: 768px) {
+    display: block;
+    positionl: absolute;
+    top: 0;
+    right: 0;
+    transform: translate(-100%, 50%);
+    font-size: 1.5rem;
+    cursor: pointer;
+    color: ${({ theme }) => theme.text_primary};
+  }
+`;
+
+export default function Navbar() {
+  return (
+    <Nav>
+      <NavContainer>
+        <NavLogo>Logo</NavLogo>
+        <MobileIcon />
+        <NavItems>
+          <NavLink>Home</NavLink>
+        </NavItems>
+        <ButtonContainer>
+          <GithubButton>GitHub</GithubButton>
+        </ButtonContainer>
+      </NavContainer>
+    </Nav>
+  );
 }
