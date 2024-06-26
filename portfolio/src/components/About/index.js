1d73fa883083258328763ba30b78a4c5a0f483ae
AAKhan4 <a.a.khan8@lancaster.ac.uk>
2024-06-26 19:10:01 +0200

    Update index.js

--- portfolio/src/components/About/index.js
+++ portfolio/src/components/About/index.js
@@ -1,5 +1,15 @@
 import React from "react";
+import * as About from "./AboutComponents";
 
 export default function Intro() {
-    return <div>Intro</div>
+    return <div id="about">
+        <About.IntroContainer>
+            <About.Bg>
+            </About.Bg>
+            <About.InnerContainer>
+                <About.LeftContainer></About.LeftContainer>
+                <About.RightContainer></About.RightContainer>
+            </About.InnerContainer>
+        </About.IntroContainer>
+    </div>
 }
