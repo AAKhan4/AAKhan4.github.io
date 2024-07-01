4f82516c526a9e1ada3a0945c502ffcfba80f741
AAKhan4 <a.a.khan8@lancaster.ac.uk>
2024-07-01 14:10:30 +0200

    Update index.js

--- portfolio/src/components/Skills/index.js
+++ portfolio/src/components/Skills/index.js
@@ -1,5 +1,27 @@
 import React from "react";
 
 export default function Skills() {
-    return <div>Skills</div>
+  return (
+    <Container id="skills">
+      <Wrapper>
+        <Title>Skills</Title>
+        <Desc></Desc>
+        <SkillsContainer>
+          {Skills.map((item) => (
+            <Skill>
+              <STitle>{item.title}</STitle>
+              <SList>
+                {item.skills.map((skill) => (
+                  <SItem>
+                    <SImage src={skill.image} />
+                    <SName>{skill.name}</SName>
+                  </SItem>
+                ))}
+              </SList>
+            </Skill>
+          ))}
+        </SkillsContainer>
+      </Wrapper>
+    </Container>
+  );
 }
