09e9e342cd07abe90361f20d5ee3ddd5720190f6
AAKhan4 <a.a.khan8@lancaster.ac.uk>
2024-07-02 15:14:36 +0200

    Initial create projects component

--- portfolio/src/components/Projects/ProjectComponents.js
+++ portfolio/src/components/Projects/ProjectComponents.js
@@ -1,1 +1,41 @@
 import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  position: relative;
+  z-index: 1;
+  align-items: center;
+`;
+
+export const Wrapper = styled.div`
+  max-width: 1100px;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  width: 100%;
+  gap: 12px;
+`;
+
+export const Title = styled.div`
+  font-size: 42px;
+  font-weight: 600;
+  text-align: center;
+  margin-top: 20px;
+  color: ${({ theme }) => theme.text_primary};
+  @media (max-width: 768px) {
+    margin-top: 12px;
+    font-size: 32px;
+  }
+`;
+
+export const Desc = styled.div`
+  font-size: 18px;
+  text-align: center;
+  color: ${({ theme }) => theme.text_secondary};
+  @media (max-width: 768px) {
+    font-size: 16px
+  }
+`;
--- portfolio/src/components/Projects/index.js
+++ portfolio/src/components/Projects/index.js
@@ -2,7 +2,12 @@ import React from "react";
 import * as P from "./ProjectComponents";
 
 export default function Projects() {
-    return (
-        <div>Projects</div>
-    );
+  return (
+    <P.Container id="projects">
+      <P.Wrapper>
+        <P.Title>Projects</P.Title>
+        <P.Desc>Some of the projects I have worked on.</P.Desc>
+      </P.Wrapper>
+    </P.Container>
+  );
 }
