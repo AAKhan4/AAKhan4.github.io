1533224972664293042964738f0ad1c15a231735
AAKhan4 <a.a.khan8@lancaster.ac.uk>
2024-07-02 23:58:00 +0200

    Update Card.js

--- portfolio/src/components/EduCards/Card.js
+++ portfolio/src/components/EduCards/Card.js
@@ -2,5 +2,19 @@ import React from "react";
 import * as C from "./CardComponents";
 
 export default function Card({ education }) {
-    return (<div>Hello</div>)
+  return (
+    <C.Container>
+      <C.Header>
+        <C.Institution>{education.institution}</C.Institution>
+        <C.Degree>{education.degree}</C.Degree>
+        <C.Date>{education.date}</C.Date>
+      </C.Header>
+      <C.Body>
+        {education.grades.map((grade) => (
+            <C.Grades>{grade}</C.Grades>
+            ))}
+        <C.Desc>{education.desc}</C.Desc>
+      </C.Body>
+    </C.Container>
+  );
 }
