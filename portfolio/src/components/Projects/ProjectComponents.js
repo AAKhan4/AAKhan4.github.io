0ec45bb245b5c0b5a10a8878c192040007d1ab00
AAKhan4 <a.a.khan8@lancaster.ac.uk>
2024-07-02 16:37:24 +0200

    Update ProjectComponents.js

--- portfolio/src/components/Projects/ProjectComponents.js
+++ portfolio/src/components/Projects/ProjectComponents.js
@@ -36,6 +36,44 @@ export const Desc = styled.div`
   text-align: center;
   color: ${({ theme }) => theme.text_secondary};
   @media (max-width: 768px) {
-    font-size: 16px
+    font-size: 16px;
   }
+`;
+
+export const ToggleGroup = styled.div`
+  display: flex;
+  width: fit-content;
+  justify-content: center;
+  align-items: center;
+  border: 1.5px solid ${({ theme }) => theme.primary};
+  color: ${({ theme }) => theme.primary};
+  font-size: 16px;
+  border-radius: 12px;
+  font-weight: 500;
+  margin: 22px auto;
+  @media (max-width: 768px) {
+    font-size: 12px;
+  }
+`;
+
+export const ToggleBtn = styled.div`
+  padding: 8px 18px;
+  border-radius: 6px;
+  cursor: pointer;
+  ${({ active, theme }) => active && `background-color: ${theme.primary + 20};`}
+  }
+  @media (max-width: 768px) {
+    padding: 6px 8px;
+    border-radius: 4px;
+  &:hover {
+    background-color: ${({ theme }) => theme.primary + 8};
+  }
+`;
+
+export const CardContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-wrap: wrap;
+  gap: 28px;
 `;
