8cd0800344032b17242534474a408ba75ab87403
AAKhan4 <a.a.khan8@lancaster.ac.uk>
2024-07-02 17:53:40 +0200

    Update Card.js

--- portfolio/src/components/ProjectCards/Card.js
+++ portfolio/src/components/ProjectCards/Card.js
@@ -1,11 +1,19 @@
 import React from "react";
+import * as C from "./CardComponents";
 
 export default function Card({ project }) {
-    return (
-        <div>
-            <h1>{project.title}</h1>
-            <p>{project.desc}</p>
-            <img src={project.image} alt={project.title} />
-        </div>
-    );
+  return (
+    <C.Card>
+      <C.Image src={project.image} alt={project.title} />
+      <C.Tags>
+        {project.tags.map((tag) => (
+          <C.Tag key={tag}>{tag}</C.Tag>
+        ))}
+      </C.Tags>
+      <C.Title>{project.title}</C.Title>
+      <C.SubTitle>{project.SubTitle}</C.SubTitle>
+      <C.Details>{project.description}</C.Details>
+      <C.Members></C.Members>
+    </C.Card>
+  );
 }
