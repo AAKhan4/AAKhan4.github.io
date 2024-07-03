63a6700ef17c64b9b86f8bd1a88293be51875027
AAKhan4 <a.a.khan8@lancaster.ac.uk>
2024-07-03 19:34:48 +0200

    Update index.js

--- portfolio/src/components/Contact/index.js
+++ portfolio/src/components/Contact/index.js
@@ -1,5 +1,22 @@
 import React from "react";
+import * as C from "./ContactComponents";
 
 export default function Contact() {
-    return <div>Contact</div>
+  return (
+    <C.Container id="contact">
+      <C.Wrapper>
+        <C.Title>Contact</C.Title>
+        <C.Desc>
+          Connect via sending an email.
+        </C.Desc>
+        <C.Form>
+            <C.Input type="text" placeholder="Name *" required />
+            <C.Input type="email" placeholder="Email  *" required />
+            <C.Input type="text" placeholder="Subject" />
+            <C.TextArea placeholder="Message *" required />
+            <C.Button>Send</C.Button>
+        </C.Form>
+      </C.Wrapper>
+    </C.Container>
+  );
 }
