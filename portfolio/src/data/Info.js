82b04a4d38cbd5162043c4919593984b36d968ad
AAKhan4 <a.a.khan8@lancaster.ac.uk>
2024-07-04 01:37:50 +0200

    Update Info.js

--- portfolio/src/data/Info.js
+++ portfolio/src/data/Info.js
@@ -4,6 +4,7 @@ import { SubTitle } from "../components/Projects/ProjectCards/CardComponents";
 export const Bio = {
   name: "Aditya Khan",
   roles: ["Software Engineering Student..."],
+  email: "aditya041004@gmail.com",
   description:
     "In my final year of study at Lancaster University. Highly motivated self-sufficient learner, dedicated to expanding my skill set through independent research & online courses both within & beyond the scope of my university curriculum. Seeking internship opportunities to apply current knowledge, learn, contribute & gain real life experience working in a professional environment.",
   github: "https://github.com/AAKhan4",
