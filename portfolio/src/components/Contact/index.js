21e77a1718f2d5d33d21170cf5aa7efa6abfa263
AAKhan4 <a.a.khan8@lancaster.ac.uk>
2024-07-04 17:11:45 +0200

    Update contact to function with emailjs

--- portfolio/src/components/Contact/index.js
+++ portfolio/src/components/Contact/index.js
@@ -1,20 +1,53 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import * as C from "./ContactComponents";
+import emailjs from "@emailjs/browser";
 
 export default function Contact() {
+  const form = useRef();
+  const [submit, setSubmit] = useState("Send");
+
+  const submitted = () => {
+    if (!form.current.checkValidity()) {
+      setSubmit("Please fill all required fields!");
+      setTimeout(() => {
+        setSubmit("Send");
+      }, 2000);
+      return;
+    }
+    setSubmit("Message Sent!");
+    setTimeout(() => {
+      setSubmit("Send");
+    }, 2000);
+  }
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm("service_eiktlvk", "template_b4ehl28", form.current, "A-ZSmBEGr8Lqmux6t")
+      .then(
+        () => {
+          console.log("SUCCESS!");
+        },
+        (error) => {
+          console.log("FAILED...", error.text);
+        }
+      );
+
+    form.current.reset();
+  };
+
   return (
     <C.Container id="contact">
       <C.Wrapper>
         <C.Title>Contact</C.Title>
-        <C.Desc>
-          Connect via sending an email.
-        </C.Desc>
-        <C.Form>
-            <C.Input type="text" placeholder="Name *" required />
-            <C.Input type="email" placeholder="Email  *" required />
-            <C.Input type="text" placeholder="Subject" />
-            <C.TextArea placeholder="Message *" required />
-            <C.Button>Send</C.Button>
+        <C.Desc>Connect via sending an email.</C.Desc>
+        <C.Form ref={form} onSubmit={sendEmail}>
+          <C.Input type="text" id="name" name="name" placeholder="Name *" required />
+          <C.Input type="email" id="email" name="email" placeholder="Email  *" required />
+          <C.Input type="text" id="subject" name="subject" placeholder="Subject" />
+          <C.TextArea id="message" name="message" placeholder="Message *" required />
+          <C.Button type="submit" onClick={submitted}>{submit}</C.Button>
         </C.Form>
       </C.Wrapper>
     </C.Container>
